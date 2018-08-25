e86bb3a01391f6f4b1f3ee598917b25fb8acea80
Osmar Ponce <krekrre14@gmail.com>
2018-08-25 14:08:50 +0000

    added stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Word of Truth Baptist Church - Prescott Valley, AZ"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
