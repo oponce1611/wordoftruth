4820acee47fb60c365e10c2eb75e3da5159963ea
Osmar Ponce <krekrre14@gmail.com>
2018-12-29 18:24:52 +0000

    deleted pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  
-  get 'sermons' => 'sermons#index'
   get '/doctrine' => 'pages#doctrine'
-  get '/about' => 'pages#about'
-  get '/contact' => 'pages#contact'
   get '/donate' => 'pages#donate'
   get '/donate/index.html' => 'pages#donate'
 
